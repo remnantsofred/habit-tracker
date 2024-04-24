14c399eda3f253ba0a23a45b9aeb03f1eca8845c
remnantsofred <remnantsofred@gmail.com>
2024-04-24 16:26:04 -0700

    Finish refactor of Countdown component

--- src/App.tsx
+++ src/App.tsx
@@ -43,15 +43,6 @@ export function App() {
         </a>
       </div>
       <h1 className='main-h1'>Ankle Progress</h1>
-      {/* <Countdown /> */}
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          {/* Edit <code>src/App.tsx</code> and save to test HMR */}
-        </p>
-      </div>
       <p className="read-the-docs">
       </p>
       <div>
--- src/components/Countdown/Countdown.tsx
+++ src/components/Countdown/Countdown.tsx
@@ -2,24 +2,35 @@ import React, { useState, useEffect} from "react";
 import './Countdown.css'
 
 // const COUNTDOWN_TARGET = new Date("2024-05-23T18:00:00");
-
 const getTimeLeft = (date) => {
-  const totalTimeLeft = date - new Date();
-  const weeks = Math.floor(totalTimeLeft / (1000 * 60 * 60 * 24 * 7) % 7);
+  // const totalTimeLeft = date - new Date();
+  const now = Date.now();
+  const laterDate = Math.max(now, date);
+  const earlierDate = Math.min(now, date);
+  let status;
+
+  laterDate === now ? status = 'past' : status = 'future';
+
+  const totalTimeLeft = Math.floor(laterDate - earlierDate);
+
+  const weeks = Math.floor(totalTimeLeft / (1000 * 60 * 60 * 24 * 7) );
   const days = Math.floor(totalTimeLeft / (1000 * 60 * 60 * 24) % 7);
   const hours = Math.floor(totalTimeLeft / (1000 * 60 * 60) % 24);
   const minutes = Math.floor(totalTimeLeft / (1000 * 60) % 60);
   const seconds = Math.floor(totalTimeLeft / (1000) % 60);
-  return { weeks, days, hours, minutes, seconds}
+  return [status, {'weeks': weeks, 'days': days, 'hours': hours, 'minutes': minutes, 'seconds': seconds} ]
 }
+
 
 const Countdown = ({milestone}) => {
   const [timeLeft, setTimeLeft] = useState(()=> getTimeLeft());
-
+  const [status, setStatus] = useState('');
+  
+  
   useEffect(() => {
     const timer = setInterval(() => {
-        setTimeLeft(getTimeLeft(milestone.date))
-      }, 1000)
+      setTimeLeft(getTimeLeft(milestone.date))
+    }, 1000)
     return ()=> {
       clearInterval(timer)
     }
@@ -27,9 +38,10 @@ const Countdown = ({milestone}) => {
 
   return (
     <div className="countdown">
-      <h2>Countdown until {milestone.name}</h2>
+      {timeLeft[0] === 'future' && <h2>Countdown until {milestone.name}</h2>}
+      {timeLeft[0] === 'past' && <h2>Countdown since {milestone.name}</h2>}
       <div className="content">
-        {Object.entries(timeLeft).map(el => {
+        {Object.entries(timeLeft[1]).map(el => {
           const label = el[0];
           const value = el[1];
           return (
