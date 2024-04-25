352ac784dd5c69cd33af2d76e54a7c16e18827c0
remnantsofred <remnantsofred@gmail.com>
2024-04-24 17:22:02 -0700

    Progress on Form component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -3,13 +3,17 @@ import './Form.css'
 const Form = () => {
   return (
     <form action="" className="form">
-      <h3>Add milestone</h3>
-      <label htmlFor="name">Name</label>
-      <input type="text" name="name" id="name" />
-      <label htmlFor="date">Date</label>
-      <input type="datetime-local" name="date" id="date" />
-      
-      <button>Submit</button>
+      <h3>New milestone</h3>
+      <div className='column left'>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" />
+        
+      </div>
+      <div className='column left'>
+        <label htmlFor="date">Date</label>
+        <input type="datetime-local" name="date" id="date" />
+      </div>
+      <button>Add</button>
     </form>
   )
 }
